ece211a27839108c9295f35da208f3a889610920
Greg Morgan <gregm9999@gmail.com>
2018-11-15 00:43:18 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,10 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects={}
+  kickstarter.css("li.project.grid_4").each do |project|
+  projects[project] = {}
 end
+projects
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
