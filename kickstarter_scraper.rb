6a634bab559815e3311eb97f5b826a767c309b8b
Greg Morgan <gregm9999@gmail.com>
2018-11-15 00:44:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,8 +8,9 @@ def create_project_hash
   projects={}
   kickstarter.css("li.project.grid_4").each do |project|
   projects[project] = {}
+  projects
 end
-projects
+
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
